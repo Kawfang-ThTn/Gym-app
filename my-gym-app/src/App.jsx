87c99122419f4c7b00ac5ea11c068e261a3d0604
Kawfang-ThTn <thanthong.somwijit@gmail.com>
2024-10-31 19:24:19 +0700

    start

--- my-gym-app/src/App.jsx
+++ my-gym-app/src/App.jsx
@@ -1,11 +1,15 @@
+import React , {useState} from 'react'
+import Generator from './components/Generator.jsx'
+import Workout from './components/Workout.jsx'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
+        <Generator />
+        <Workout />
+      </main>
     </>
   )
 }
