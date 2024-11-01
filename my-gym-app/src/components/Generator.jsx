09a50d4c35377d1a608a351e52b5463c95c43dca
Kawfang-ThTn <thanthong.somwijit@gmail.com>
2024-11-01 15:27:16 +0700

    update generator.jsx / section muscle

--- my-gym-app/src/components/Generator.jsx
+++ my-gym-app/src/components/Generator.jsx
@@ -1,59 +1,142 @@
-import React from 'react'
-import SectionWrapper from './SectionWrapper'
-import { WORKOUTS } from '../utilities/detail';
+import React, { useState } from "react";
+import SectionWrapper from "./SectionWrapper";
+import { SCHEMES, WORKOUTS } from "../utilities/detail";
 
+function Header(props) {
+  const { index, title, description } = props;
 
-function Header(props){
-
-  const { index , title , description } = props
-
-  return(
+  return (
     <>
-      <div className='flex flex-col gap-4'>
-
-        <div className='flex items-center gap-2 justify-center'>
-
-          <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400'>{index}</p>
-          <h4 className='text-xl sm:text-2xl md:text-3xl'>{title}</h4>
-
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center gap-2 justify-center">
+          <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400">
+            {index}
+          </p>
+          <h4 className="text-xl sm:text-2xl md:text-3xl">{title}</h4>
         </div>
 
-          <p className='text-sm sm:text-base mx-auto'>{description}</p>
-      
+        <p className="text-sm sm:text-base mx-auto">{description}</p>
       </div>
     </>
   );
 }
 
 function Generator() {
+  const [showModal, setShowModal] = useState(false);
+  const [poison, setPoison] = useState("individual");
+  const [muscles, setMuscles] = useState([]);
+  const [goals, setGoals] = useState("strength_power");
+
+  function toggleModal() {
+    setShowModal(!showModal);
+  }
+
+  function updateMuscle(muscleGroup) {
+
+    if (muscles.length > 2) {
+      return
+    }
+
+    if (poison !== 'individual') {
+      setMuscles([muscleGroup])
+      return
+    }
+
+    if (muscles.includes(muscleGroup)) {
+      setMuscles(muscles.filter( val => val !== muscleGroup))
+      return
+    }
+
+    setMuscles([...muscles , muscleGroup])
+
+  }
+
   return (
     <>
-        <SectionWrapper header={"generate your workout"} title={["It\'s","Huge","o\'clock"]}>
+      <SectionWrapper
+        header={"generate your workout"}
+        title={["It's", "Huge", "o'clock"]}
+      >
+        <Header
+          index={"01"}
+          title={"Pick your poison"}
+          description={"Select the workout you wish to endure."}
+        />
 
-          <Header index={'01'} title={'Pick your poison'} description={'Select the workout you wish to endure.'} />
+        <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+          {Object.keys(WORKOUTS).map((type, typeIndex) => {
+            return (
+              <button
+                onClick={() => {
+                  setPoison(type);
+                }}
+                className={`bg-slate-950 border py-3 rounded-lg duration-200 ${
+                  type === poison ? "border-blue-600" : "border-blue-400"
+                }`}
+                key={typeIndex}
+              >
+                <p className="capitalize">{type.replaceAll("_", " ")}</p>
+              </button>
+            );
+          })}
+        </div>
 
-          <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
+        <Header
+          index={"02"}
+          title={"Lock on targets"}
+          description={"Select the muscles judged for annihilation."}
+        />
 
-            {Object.keys(WORKOUTS).map((type , typeIndex) => {
-              return (
-                <button className='bg-slate-950 border border-blue-400 py-3 rounded-lg duration-200 hover:border-blue-600' key={typeIndex}>
-                  <p className='capitalize'>{type.replaceAll('_'," ")}</p>
-                </button>
-              )
-            })}
-
-          </div>
-
-          <Header index={'02'} title={'Lock on targets'} description={'Select the muscles judged for annihilation.'} />
-
-          <div className='bg-slate-950 p-3 border border-solid border-blue-400 rounded-lg'>
+        <div className="bg-slate-950  border border-solid border-blue-400 rounded-lg flex flex-col">
+          <button
+            onClick={toggleModal}
+            className="relative p-3 flex items-center justify-center"
+          >
             <p>Select muscle groups</p>
-            <i className="fa-solid fa-caret-down"></i>
-          </div>
+            <i className="fa-solid fa-caret-down absolute right-3 top-1/2 -translate-y-1/2"></i>
+          </button>
+          {showModal && (
+            <div className="flex flex-col p-3">
+              {(poison === "individual"
+                ? WORKOUTS[poison]
+                : Object.keys(WORKOUTS[poison])
+              ).map((muscleGroup, muscleGroupIndex) => {
+                return (
+                  <button onClick={ () => updateMuscle(muscleGroup) } key={muscleGroupIndex} className={`hover:text-blue-400 duration-200 ${(muscles.includes(muscleGroup) ? 'text-blue-400' : ' ' )}`}>
+                    <p className='uppercase'>{muscleGroup.replaceAll('_',' ')}</p>
+                  </button>
+                );
+              })}
+            </div>
+          )}
+        </div>
 
-        </SectionWrapper>
+        <Header
+          index={"03"}
+          title={"Become Juggernaut"}
+          description={"Select your ultimate objective."}
+        />
+
+        <div className="grid grid-cols-3 gap-4">
+          {Object.keys(SCHEMES).map((scheme, schemeIndex) => {
+            return (
+              <button
+                onClick={() => {
+                  setGoals(scheme);
+                }}
+                className={`bg-slate-950 border py-3 rounded-lg duration-200 ${
+                  scheme === goals ? "border-blue-600" : "border-blue-400"
+                }`}
+                key={schemeIndex}
+              >
+                <p className="capitalize">{scheme.replaceAll("_", " ")}</p>
+              </button>
+            );
+          })}
+        </div>
+      </SectionWrapper>
     </>
-  )
+  );
 }
 
-export default Generator
+export default Generator;
